16e7afb115f71cbba1b1c2a290a97022cc8a013e
shong <shong1332@ryencatchers.com>
2022-02-02 18:33:29 +0900

    24-26

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
+import Refactoring from "./refactoring/refactoring";
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					test
-				</a>
-			</header>
-		</div>
-	);
+	return <Refactoring />;
 }
 
 export default App;
