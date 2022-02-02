cbe940c1b7038fe3f15d0c834d3788b5730c3201
shong <shong1332@ryencatchers.com>
2022-02-02 23:24:33 +0900

    p67

--- src/refactoring/CreateStatementData.js
+++ src/refactoring/CreateStatementData.js
@@ -1,25 +1,28 @@
-function createStatementData(invoice, plays) {
-	const statementData = {};
-	statementData.customer = invoice.customer;
-	statementData.performances = invoice.performances.map(enrichPerformance);
-	statementData.totalAmount = totalAmount(statementData);
-	statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-	return statementData;
+class PerformanceCalculator {
+	constructor(aPerformance, aPlay) {
+		this.performance = aPerformance;
+		this.play = aPlay;
+	}
+}
+
+export default function createStatementData(invoice, plays) {
+	const result = {};
+	result.customer = invoice.customer;
+	result.performances = invoice.performances.map(enrichPerformance);
+	result.totalAmount = totalAmount(result);
+	result.totalVolumeCredits = totalVolumeCredits(result);
+	return result;
 
 	function enrichPerformance(aPerformance) {
+		const calculator = new PerformanceCalculator(
+			aPerformance,
+			playFor(aPerformance)
+		);
 		const result = Object.assign({}, aPerformance);
-		result.play = playFor(result);
+		result.play = calculator.play;
 		result.amount = amountFor(result);
 		result.volumeCredits = volumeCreditsFor(result);
 		return result;
-	}
-
-	function totalAmount(data) {
-		return data.performances.reduce((total, p) => total + p.amount, 0);
-	}
-
-	function totalVolumeCredits(data) {
-		return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
 	}
 
 	function playFor(aPerformance) {
@@ -47,11 +50,20 @@ function createStatementData(invoice, plays) {
 		}
 		return result;
 	}
+
 	function volumeCreditsFor(aPerformance) {
 		let result = 0;
 		result += Math.max(aPerformance.audience - 30, 0);
 		if ("comedy" === aPerformance.play.type)
 			volumeCredits += Math.floor(aPerformance.audience / 5);
 		return result;
+	}
+
+	function totalAmount(data) {
+		return data.performances.reduce((total, p) => total + p.amount, 0);
+	}
+
+	function totalVolumeCredits(data) {
+		return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
 	}
 }
