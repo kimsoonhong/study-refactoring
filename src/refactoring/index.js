1637050c72cdc3ffafdecdfd3485a2832385dce8
shong <shong1332@ryencatchers.com>
2022-02-02 22:39:48 +0900

    beforeHTML

--- src/refactoring/index.js
+++ src/refactoring/index.js
@@ -1,56 +1,29 @@
 import INVOICE from "../invoices.json";
 import PLAYS from "../plays.json";
+import createStatementData from "./CreateStatementData";
 
 function statement(invoice, plays) {
-	let totalAmount = 0;
-	let volumeCredits = 0;
-	let result = `청구 내역(고객명: ${invoice.customer})\n`;
-	const format = new Intl.NumberFormat("en-US", {
-		style: "currency",
-		currency: "USD",
-		minimumFractionDigits: 2,
-	}).format;
+	return renderPlainText(createStatementData(invoice, plays), plays);
 
-	for (let perf of invoice.performances) {
-		const play = plays[perf.playID]; // object {name, type}
-		let thisAmount = amountFor(perf, play);
+	function renderPlainText(data, plays) {
+		let result = `청구 내역(고객명: ${data.customer})\n`;
 
-		function amountFor(perf, play) {
-			let thisAmount = 0;
-			switch (play.type) {
-				case "tragedy": //비극
-					thisAmount = 40000;
-					if (perf.audience > 30) {
-						thisAmount += 1000 * (perf.audience - 30);
-					}
-					break;
-				case "comedy": //희극
-					thisAmount = 30000;
-					if (perf.audience > 20) {
-						thisAmount += 1000 + 500 * (perf.audience - 20);
-					}
-					thisAmount += 300 * perf.audience;
-					break;
-				default:
-					throw new Error(`알 수 없는 장르: ${play.type}`);
-			}
-			return thisAmount;
+		for (let perf of data.performances) {
+			result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
 		}
 
-		// 포인트를 적립한다.
-		volumeCredits += Math.max(perf.audience - 30, 0);
-		// 희극 관객 5명마다 추가 포인트를 제공한다.
-		if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+		result += `총액: ${usd(data.totalAmount())}\n`;
+		result += `적립 포인트: ${data.totalVolumeCredits()}점\n`;
+		return result;
 
-		// 청구 내역을 출력한다.
-		result += `${play.name}: ${format(thisAmount / 100)} (${
-			perf.audience
-		}석)\n`;
-		totalAmount += thisAmount;
+		function usd(aNumber) {
+			return new Intl.NumberFormat("en-US", {
+				style: "currency",
+				currency: "USD",
+				minimumFractionDigits: 2,
+			}).format(aNumber / 100);
+		}
 	}
-	result += `총액: ${format(totalAmount / 100)}\n`;
-	result += `적립 포인트: ${volumeCredits}점\n`;
-	return result;
 }
 
-statement(INVOICE[0], PLAYS);
+statement(INVOICE, PLAYS);
